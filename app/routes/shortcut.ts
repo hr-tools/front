a75f5e6c9cc58fd8b61dddd6a601748c9e5285e2
shay <bakkcat@gmail.com>
2022-09-08 22:28:11 +0200

    iCloud shortcut link

--- app/routes/shortcut.ts
+++ app/routes/shortcut.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@remix-run/server-runtime'
 
 export function loader() {
-    return redirect('/')
+    return redirect('https://www.icloud.com/shortcuts/0d007c6b254d4adfb60c8605152cd58d')
 }
