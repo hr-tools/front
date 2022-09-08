36828d814913e3c91d0cc83238a7d3a796fa81df
shay <bakkcat@gmail.com>
2022-09-08 23:59:25 +0200

    Update shortcut URL to patch 1.0.3 1.0.2 was never pushed here

--- app/routes/shortcut.ts
+++ app/routes/shortcut.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@remix-run/server-runtime'
 
 export function loader() {
-    return redirect('https://www.icloud.com/shortcuts/3a7d93c13ef444eca387fd1fda7a79e5')
+    return redirect('https://www.icloud.com/shortcuts/1ebdf49191a644acbcf3786e3ae68310')
 }
